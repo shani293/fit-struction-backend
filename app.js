783967616b9d869890e5fbd4f725ab82748dd1c2
shani293 <muhammadzeeshan9899@gmail.com>
2022-03-03 18:29:57 +0500

    path add

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/fit-struction/', indexRouter);
+app.use('/fit-struction/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
