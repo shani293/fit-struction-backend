65d855989115275165d64ef70810647e7c385f83
shani293 <muhammadzeeshan9899@gmail.com>
2022-05-21 02:56:17 +0500

    shorter length token

--- helper/generateToken.js
+++ helper/generateToken.js
@@ -1,8 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = async function generateToken(body) {
+    let specificData = {
+        _id: body?._id,
+        email: body?.email
+    }
     let token = jwt.sign({
-        data: body
+        data: specificData
     }, "fit-struction")
     return token
 }
