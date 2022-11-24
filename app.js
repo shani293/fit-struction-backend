04b5d66f4291a09a71b58d7f901024cb471054c3
shani293 <muhammadzeeshan9899@gmail.com>
2022-11-24 21:39:54 +0500

    update path

--- app.js
+++ app.js
@@ -23,11 +23,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/fit-struction/', indexRouter);
-// app.use('/fit-struction/users', usersRouter);
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/file', upload);
+app.use('/fit-struction/', indexRouter);
+app.use('/fit-struction/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/file', upload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
